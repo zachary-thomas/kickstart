a70080c6977e3a98ee0bd68e0cb1a2fe571e9833
Zachary M Thomas <thomazac@mail.gvsu.edu>
2023-10-17 18:20:08 -0500

    change compile

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -11,9 +11,11 @@ const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
 
+console.log(output)
+
 for (let contract in output){
     fs.outputJsonSync(
-        path.resolve(buildPath, contract + '.json'),
+        path.resolve(buildPath, contract.replace(':', '') + '.json'),
         output[contract]
     );
 }
