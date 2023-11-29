964e2aac4b2e8a6b5def2b5569edcb4c33dfbf14
Zachary M Thomas <thomazac@mail.gvsu.edu>
2023-11-29 17:54:38 -0600

    add on to test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -33,10 +33,23 @@ beforeEach(async () => {
     );
 });
 
-describe('Campaigns', () => {
-    it('deploys a factory and a campaign', () => {
-        assert.ok(factory.options.address);
-        assert.ok(campaign.options.address);
+describe("Campaigns", () => {
+    it("deploys a factory and a campaign", () => {
+      assert.ok(factory.options.address);
+      assert.ok(campaign.options.address);
     });
-})
-
+  
+    it("marks caller as the campaign manager", async () => {
+      const manager = await campaign.methods.manager().call();
+      assert.equal(accounts[0], manager);
+    });
+  
+    it("allows people to contribute money and marks them as approvers", async () => {
+      await campaign.methods.contribute().send({
+        value: "200",
+        from: accounts[1],
+      });
+      const isContributor = await campaign.methods.approvers(accounts[1]).call();
+      assert(isContributor);
+    });
+  });
